0dae992c4a3d628219f639798bc437aebe52847e
andreea.florescu <andreea.florescu@cti.pub.ro>
2014-08-19 23:12:52 +0300

    integrated parse app

--- PartyCookie/src/com/cran/partycookie/MainActivity.java
+++ PartyCookie/src/com/cran/partycookie/MainActivity.java
@@ -1,15 +1,21 @@
 package com.cran.partycookie;
+import com.parse.Parse;
+import com.parse.PushService;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	private String applicationId = new String("ywQHYRUuJfyB8yIzY6G3hP1rMS98QWzoCUgasoki");
+	private String clientKey = new String("GmsXizv3jyLV4ks4RBMIQ8N9nM9X825Da8YAEvuI");
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Parse.initialize(this, applicationId, clientKey);
+		PushService.setDefaultPushCallback(this, MainActivity.class);
 	}
 
 	@Override
